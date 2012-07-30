9bf507d088b3f3f30fa929e1b030f95ab1c40a3c
Adam Jahn <ajjahn@gmail.com>
2012-07-30 14:33:52 -0400

    group and owner of bind configs should be bind

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,15 +2,15 @@ class dns::config {
 
   file { "/etc/bind":
     ensure => directory,
-    owner => "root",
-    group => "root",
+    owner => "bind",
+    group => "bind",
     mode => 0755,
   }
   
   file { "/etc/bind/named.conf":
     ensure => present,
-    owner => "root",
-    group => "root",
+    owner => "bind",
+    group => "bind",
     mode => 0644,
     require => [File["/etc/bind"], Class["dns::install"]],
     notify => Class["dns::service"],
