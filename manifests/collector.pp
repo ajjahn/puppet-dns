d563f8b52fb0064029e69f49f89e35ad832723f5
Adam Jahn <ajjahn@gmail.com>
2012-08-02 22:08:18 -0400

    export dns records to server

---
+++ manifests/collector.pp
@@ -0,0 +1,6 @@
+class dns::collector {
+  Member <<| |>> {
+    require => Class["dns::server"],
+    notify => Class["dns::server::service"]
+  }
+}
---
+++ manifests/member.pp
@@ -0,0 +1,15 @@
+class dns::member {
+  @@member { "${::fqdn}":
+    domain => "${::domain}",
+    hostname => "${::hostname}",
+    ipaddress => "${ipaddress}"
+  }
+}
+
+define member ($domain, $hostname, $ipaddress) {
+  dns::record::a { $hostname:
+    zone => $domain,
+    data => $ipaddress,
+    ptr => true;
+  }
+}
