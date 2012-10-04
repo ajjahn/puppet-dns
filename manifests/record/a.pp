6f53fba69ad5346ffc8e15d88b1f70b579544e4c
Guillaume Rose <guillaume.rose@gmail.com>
2012-10-04 18:26:25 +0300

    Fix PTR record creation

--- manifests/record/a.pp
+++ manifests/record/a.pp
@@ -11,7 +11,7 @@ define dns::record::a ($zone, $data, $ttl = '', $ptr = false) {
 
   if $ptr {
     $ip = inline_template('<%= data.kind_of?(Array) ? data.first : data %>')
-    $reverse_zone = inline_template('<%= ip.split(".")[0..-2].reverse.join(".") %>')
+    $reverse_zone = inline_template('<%= ip.split(".")[0..-2].reverse.join(".") %>.IN-ADDR.ARPA')
     $octet = inline_template('<%= ip.split(".")[-1] %>')
 
     dns::record::ptr { $octet:
