5231da7f50a31b17ee67365fcd7b638a704d34b6
Steffen L. Norgren <github@trollop.org>
2012-10-23 19:32:49 -0700

    Cleaning puppet lint
    double_quoted_strings:WARNING:double quoted string containing no
    variables

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -1,34 +1,34 @@
 class dns::server::config {
 
-  file { "/etc/bind":
+  file { '/etc/bind':
     ensure => directory,
-    owner => "bind",
-    group => "bind",
+    owner => 'bind',
+    group => 'bind',
     mode => 0755,
   }
 
-  file { "/etc/bind/named.conf":
+  file { '/etc/bind/named.conf':
     ensure => present,
-    owner => "bind",
-    group => "bind",
-    mode => 0644,
-    require => [File["/etc/bind"], Class["dns::server::install"]],
-    notify => Class["dns::server::service"],
-  }
-
-  concat { "/etc/bind/named.conf.local":
     owner => 'bind',
     group => 'bind',
     mode => 0644,
-    require => Class["concat::setup"],
-    notify => Class["dns::server::service"]
+    require => [File['/etc/bind'], Class['dns::server::install']],
+    notify => Class['dns::server::service'],
   }
 
-  concat::fragment{"named.conf.local.header":
-     target  => "/etc/bind/named.conf.local",
+  concat { '/etc/bind/named.conf.local':
+    owner => 'bind',
+    group => 'bind',
+    mode => 0644,
+    require => Class['concat::setup'],
+    notify => Class['dns::server::service']
+  }
+
+  concat::fragment{'named.conf.local.header':
+     target  => '/etc/bind/named.conf.local',
      order   => 1,
      ensure => present,
-     content => "// File managed by Puppet.\n"
+     content => '// File managed by Puppet.\n'
   }
 
 }
