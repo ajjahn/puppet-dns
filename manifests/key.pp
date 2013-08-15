bfce1ba32d6ad884533589911a8c01ef8f7a2700
Peter J. Pouliot <ppouliot@gmail.com>
2013-08-15 12:40:23 -0400

    generating proper key files

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,30 +1,34 @@
 define dns::key {
 
-  notify { Exec["dnssec-keygen-${name}"]:} 
-
-  file {"/tmp/${name}-secret.sh":
+  file {"/tmp/${name}-secret.sh": 
     ensure => file,
-    require => File["/etc/bind/bind.keys.d"],
-    content => template('bind/secret')
+    mode    => '0777',
+    content => template('dns/secret.erb'),
+    notify => Exec["dnssec-keygen-${name}"],
   }
+    
 
   exec {"dnssec-keygen-${name}":
     command     => "/usr/sbin/dnssec-keygen -a HMAC-MD5 -r /dev/urandom -b 128 -n USER ${name}",
     cwd         => '/etc/bind/bind.keys.d',
     require     => [Package['dnssec-tools','bind9'],File['/etc/bind/bind.keys.d']],
     refreshonly => true,
-    notify => Exec["get-secret-from-${name}"]
+    notify => Exec["get-secret-from-${name}"],
   }
 
   exec {"get-secret-from-${name}":
-    command => "SECRET=`cat /etc/bind/bind.keys.d/K${name}.+*+*.key |tr -s \" \"|cut -d \" \" -f7` cat << EOF > echo \'secret \"\' $SECRET \'\"\' > ${name}.secret",
+    command => "/tmp/${name}-secret.sh",
     cwd         => '/etc/bind/bind.keys.d',
     creates     => "/etc/bind/bind.keys.d/${name}.secret",
-    require     => [Package['dnssec-tools','bind9'],File['/etc/bind/bind.keys.d']],
+    require     => [Exec["dnssec-keygen-${name}"],File['/etc/bind/bind.keys.d',"/tmp/${name}-secret.sh"]],
     refreshonly => true,
   }
+ 
+  file { "/etc/bind/bind.keys.d/${name}.secret":
+    require => Exec["get-secret-from-${name}"],
+  }
 
-  concat { "/etc/bind/bind.key.d/$name.key":
+  concat { "/etc/bind/bind.keys.d/${name}.key":
     owner   => 'bind',
     group   => 'bind',
     mode    => '0644',
@@ -32,25 +36,32 @@ define dns::key {
     notify  => Class['dns::server::service']
   }
 
-  concat::fragment { "$name.key-header":
+  concat::fragment { "${name}.key-header":
     ensure  => present,
-    target  => "/etc/bind/bind.key.d/$name.key",
+    target  => "/etc/bind/bind.keys.d/${name}.key",
     order   => 1,
     content => template('dns/key.erb'),
-    require => Exec["dnssec-keygen-${name}"],
+    require => [Exec["get-secret-from-${name}"], File["/etc/bind/bind.keys.d/${name}.secret"]],
   }
-  concat::fragment { "$name.key-secret":
+  concat::fragment { "${name}.key-secret":
     ensure  => present,
-    target  => "/etc/bind/bind.key.d/$name.key",
+    target  => "/etc/bind/bind.keys.d/${name}.key",
     order   => 2,
+#    content => template("/etc/bind/bind.keys.d/${name}.secret"),
     source  => "/etc/bind/bind.keys.d/${name}.secret",
-    require => [Exec["dnssec-keygen-${name}"],File ["/etc/bind/bind.keys.d/${name}.secret"]],
+    require => [ Exec[ "get-secret-from-${name}" ], File["/etc/bind/bind.keys.d/${name}.secret"]],
   }
-  concat::fragment { "$name.key-footer":
+  concat::fragment { "${name}.key-footer":
     ensure  => present,
-    target  => "/etc/bind/bind.key.d/$name.key",
+    target  => "/etc/bind/bind.keys.d/${name}.key",
     order   => 3,
     content => '}:', 
-    require => Exec["dnssec-keygen-${name}"],
+    require => [Exec["get-secret-from-${name}"], File["/etc/bind/bind.keys.d/${name}.secret"]],
   }
+  #concat::fragment{"named.conf.local.${name}.key":
+  #  ensure  => present,
+  #  target  => '/etc/bind/named.conf.local',
+  #  content => templates
+  #}
+
 }
