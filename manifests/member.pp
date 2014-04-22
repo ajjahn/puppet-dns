1aafd9d373864759c3b1111add0708c880d7b45f
David Danzilio <david.danzilio@mac.com>
2014-04-21 23:17:45 -0600

    This will break the member class, but I'm not entirely clear on its function, this would require a semver bump

--- manifests/member.pp
+++ manifests/member.pp
@@ -1,11 +1,3 @@
-class dns::member {
-  @@member { $::fqdn:
-    domain    => $::domain,
-    hostname  => $::hostname,
-    ipaddress => $::ipaddress
-  }
-}
-
 define dns::member ($domain, $hostname, $ipaddress) {
   dns::record::a { $hostname:
     zone => $domain,
