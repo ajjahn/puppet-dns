929a0b8cd150af618bb77e0c000b9ebb5f3c2b3b
Johnson Earls <johnson.earls@oracle.com>
2015-09-30 07:53:59 -0700

    Properly escape the { and } in the listen-on-v6 regexp check.

--- spec/defines/dns__server__options_spec.rb
+++ spec/defines/dns__server__options_spec.rb
@@ -87,7 +87,7 @@ describe 'dns::server::options', :type => :define do
 
   context 'when the listen_on_ipv6 option is not provided' do
     let(:params) { {} }
-    it { should contain_file('/etc/bind/named.conf.options').with_content(/listen-on-v6 {.+?any;.+?}/) }
+    it { should contain_file('/etc/bind/named.conf.options').with_content(/listen-on-v6 \{.+?any;.+?\}/) }
   end
 
   context 'passing a string to recursion' do
