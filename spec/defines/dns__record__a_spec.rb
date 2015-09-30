63b9ddf657c2202d9b3510abbb0a803bc8aafdb8
Johnson Earls <johnson.earls@oracle.com>
2015-09-30 06:54:55 -0700

    Add `all` and `first` values to `ptr` parameter to `dns::record::a` record type
    This updates the `ptr` parameter of the `dns::record::a` type to allow `first` or `all` values.  `first` is the same as `true` (or any other `str2bool` true values) and will act as the traditional `ptr` parameter did.  With `all`, a `dns::record::ptr` resource is created for all of the ip addresses in `data`, not just the first.  This uses a private defined type `dns_reverse_ptr_record` to actually create the `dns::record::ptr` resources so that we can get iteration in the older puppet parsers.
    
    This also creates a set of rspec tests for the `dns::record::a` defined type.

---
+++ spec/defines/dns__record__a_spec.rb
@@ -0,0 +1,82 @@
+require 'spec_helper'
+
+describe 'dns::record::a', :type => :define do
+  let(:title) { 'atest' }
+  let(:facts) { { :concat_basedir => '/tmp' } }
+
+  context 'passing a single ip address with ptr=>false' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => '192.168.128.42',
+        :ptr => false,
+    } end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.example.com.atest,A,example.com.record').with_content(/^atest\s+IN\s+A\s+192\.168\.128\.42$/) }
+    it { should_not contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.42.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record') }
+  end
+
+  context 'passing a single ip address with ptr=>true' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => '192.168.128.42',
+        :ptr => true,
+    } end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.example.com.atest,A,example.com.record').with_content(/^atest\s+IN\s+A\s+192\.168\.128\.42$/) }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.42.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^42\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing a single ip address with ptr=>all' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => '192.168.128.42',
+        :ptr => 'all',
+    } end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.example.com.atest,A,example.com.record').with_content(/^atest\s+IN\s+A\s+192\.168\.128\.42$/) }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.42.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^42\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing multiple ip addresses with ptr=>false' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => false,
+    } end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.example.com.atest,A,example.com.record').with_content(/^atest\s+IN\s+A\s+192\.168\.128\.68\natest\s+IN\s+A\s+192\.168\.128\.69\natest\s+IN\s+A\s+192\.168\.128\.70$/) }
+    it { should_not contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record') }
+  end
+
+  context 'passing multiple ip addresses with ptr=>true' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => true,
+    } end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.example.com.atest,A,example.com.record').with_content(/^atest\s+IN\s+A\s+192\.168\.128\.68\natest\s+IN\s+A\s+192\.168\.128\.69\natest\s+IN\s+A\s+192\.168\.128\.70$/) }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^68\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing multiple ip addresses with ptr=>all' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.example.com.atest,A,example.com.record').with_content(/^atest\s+IN\s+A\s+192\.168\.128\.68\natest\s+IN\s+A\s+192\.168\.128\.69\natest\s+IN\s+A\s+192\.168\.128\.70$/) }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^68\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.69.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^69\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.70.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^70\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+end
+
