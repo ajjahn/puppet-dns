ff518fb45978239bbe31fa5ce9d3e267013b0e61
Johnson Earls <johnson.earls@gmail.com>
2016-05-19 21:05:48 +0000

    allow use of class A or class B reverse zones

--- manifests/record/ptr/by_ip.pp
+++ manifests/record/ptr/by_ip.pp
@@ -74,8 +74,33 @@ define dns::record::ptr::by_ip (
   $ttl = undef ,
   $ip = $name ) {
 
-  $reverse_zone = inline_template('<%= @ip.split(".")[0..-2].reverse.join(".") %>.IN-ADDR.ARPA')
-  $octet = inline_template('<%= @ip.split(".")[-1] %>')
+  # split the IP address up into three host/zone pairs based on class A, B, or C splits:
+  # For IP address A.B.C.D,
+  # class C => host D / zone C.B.A.IN-ADDR.ARPA
+  # class B => host D.C / zone B.A.IN-ADDR.ARPA
+  # class A => host D.C.B / zone A.IN-ADDR.ARPA
+  $class_C_zone = inline_template('<%= @ip.split(".").reverse()[1..3].join(".") %>.IN-ADDR.ARPA')
+  $class_C_host = inline_template('<%= @ip.split(".").reverse()[0..0].join(".") %>')
+  $class_B_zone = inline_template('<%= @ip.split(".").reverse()[2..3].join(".") %>.IN-ADDR.ARPA')
+  $class_B_host = inline_template('<%= @ip.split(".").reverse()[0..1].join(".") %>')
+  $class_A_zone = inline_template('<%= @ip.split(".").reverse()[3..3].join(".") %>.IN-ADDR.ARPA')
+  $class_A_host = inline_template('<%= @ip.split(".").reverse()[0..2].join(".") %>')
+
+  # choose the most specific defined reverse zone file (class C, then B, then A).
+  # Default to class C if none are defined.
+  if defined(Dns::Zone[$class_C_zone]) {
+    $reverse_zone = $class_C_zone
+    $octet = $class_C_host
+  } elsif defined(Dns::Zone[$class_B_zone]) {
+    $reverse_zone = $class_B_zone
+    $octet = $class_B_host
+  } elsif defined(Dns::Zone[$class_A_zone]) {
+    $reverse_zone = $class_A_zone
+    $octet = $class_A_host
+  } else {
+    $reverse_zone = $class_C_zone
+    $octet = $class_C_host
+  }
 
   dns::record::ptr { "${octet}.${reverse_zone}":
     host => $octet,
--- spec/defines/dns__record__a_spec.rb
+++ spec/defines/dns__record__a_spec.rb
@@ -78,5 +78,108 @@ describe 'dns::record::a', :type => :define do
     it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.70.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^70\s+IN\s+PTR\s+atest\.example\.com\.$/) }
   end
 
+  context 'passing ptr=>true with class A network defined' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    let :pre_condition do [
+        'dns::zone { "192.IN-ADDR.ARPA": }',
+    ] end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,192.IN-ADDR.ARPA.record').with_content(/^68\.128\.168\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing ptr=>true with class B network defined' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    let :pre_condition do [
+        'dns::zone { "168.192.IN-ADDR.ARPA": }',
+    ] end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,168.192.IN-ADDR.ARPA.record').with_content(/^68\.128\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing ptr=>true with class C network defined' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    let :pre_condition do [
+        'dns::zone { "128.168.192.IN-ADDR.ARPA": }',
+    ] end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^68\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing ptr=>true with class A and class B network defined' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    let :pre_condition do [
+        'dns::zone { "192.IN-ADDR.ARPA": }',
+        'dns::zone { "168.192.IN-ADDR.ARPA": }',
+    ] end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,168.192.IN-ADDR.ARPA.record').with_content(/^68\.128\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing ptr=>true with class A and class C network defined' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    let :pre_condition do [
+        'dns::zone { "192.IN-ADDR.ARPA": }',
+        'dns::zone { "128.168.192.IN-ADDR.ARPA": }',
+    ] end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^68\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing ptr=>true with class B and class C network defined' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    let :pre_condition do [
+        'dns::zone { "168.192.IN-ADDR.ARPA": }',
+        'dns::zone { "128.168.192.IN-ADDR.ARPA": }',
+    ] end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^68\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
+  context 'passing ptr=>true with class A, class B and class C network defined' do
+    let :params do {
+        :host => 'atest',
+        :zone => 'example.com',
+        :data => [ '192.168.128.68', '192.168.128.69', '192.168.128.70' ],
+        :ptr => 'all',
+    } end
+    let :pre_condition do [
+        'dns::zone { "192.IN-ADDR.ARPA": }',
+        'dns::zone { "168.192.IN-ADDR.ARPA": }',
+        'dns::zone { "128.168.192.IN-ADDR.ARPA": }',
+    ] end
+    it { should_not raise_error }
+    it { should contain_concat__fragment('db.128.168.192.IN-ADDR.ARPA.68.128.168.192.IN-ADDR.ARPA,PTR,128.168.192.IN-ADDR.ARPA.record').with_content(/^68\s+IN\s+PTR\s+atest\.example\.com\.$/) }
+  end
+
 end
 
