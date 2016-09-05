a32920c17e765faf1ed7d9e2efd7d0802c190133
Johnson Earls <johnson.earls@gmail.com>
2016-09-05 16:18:46 -0700

    Make lint happy:  change variables class_[ABC]_* to class_[abc]_*

--- manifests/record/ptr/by_ip.pp
+++ manifests/record/ptr/by_ip.pp
@@ -79,27 +79,27 @@ define dns::record::ptr::by_ip (
   # class C => host D / zone C.B.A.IN-ADDR.ARPA
   # class B => host D.C / zone B.A.IN-ADDR.ARPA
   # class A => host D.C.B / zone A.IN-ADDR.ARPA
-  $class_C_zone = inline_template('<%= @ip.split(".").reverse()[1..3].join(".") %>.IN-ADDR.ARPA')
-  $class_C_host = inline_template('<%= @ip.split(".").reverse()[0..0].join(".") %>')
-  $class_B_zone = inline_template('<%= @ip.split(".").reverse()[2..3].join(".") %>.IN-ADDR.ARPA')
-  $class_B_host = inline_template('<%= @ip.split(".").reverse()[0..1].join(".") %>')
-  $class_A_zone = inline_template('<%= @ip.split(".").reverse()[3..3].join(".") %>.IN-ADDR.ARPA')
-  $class_A_host = inline_template('<%= @ip.split(".").reverse()[0..2].join(".") %>')
+  $class_c_zone = inline_template('<%= @ip.split(".").reverse()[1..3].join(".") %>.IN-ADDR.ARPA')
+  $class_c_host = inline_template('<%= @ip.split(".").reverse()[0..0].join(".") %>')
+  $class_b_zone = inline_template('<%= @ip.split(".").reverse()[2..3].join(".") %>.IN-ADDR.ARPA')
+  $class_b_host = inline_template('<%= @ip.split(".").reverse()[0..1].join(".") %>')
+  $class_a_zone = inline_template('<%= @ip.split(".").reverse()[3..3].join(".") %>.IN-ADDR.ARPA')
+  $class_a_host = inline_template('<%= @ip.split(".").reverse()[0..2].join(".") %>')
 
   # choose the most specific defined reverse zone file (class C, then B, then A).
   # Default to class C if none are defined.
-  if defined(Dns::Zone[$class_C_zone]) {
-    $reverse_zone = $class_C_zone
-    $octet = $class_C_host
-  } elsif defined(Dns::Zone[$class_B_zone]) {
-    $reverse_zone = $class_B_zone
-    $octet = $class_B_host
-  } elsif defined(Dns::Zone[$class_A_zone]) {
-    $reverse_zone = $class_A_zone
-    $octet = $class_A_host
+  if defined(Dns::Zone[$class_c_zone]) {
+    $reverse_zone = $class_c_zone
+    $octet = $class_c_host
+  } elsif defined(Dns::Zone[$class_b_zone]) {
+    $reverse_zone = $class_b_zone
+    $octet = $class_b_host
+  } elsif defined(Dns::Zone[$class_a_zone]) {
+    $reverse_zone = $class_a_zone
+    $octet = $class_a_host
   } else {
-    $reverse_zone = $class_C_zone
-    $octet = $class_C_host
+    $reverse_zone = $class_c_zone
+    $octet = $class_c_host
   }
 
   dns::record::ptr { "${octet}.${reverse_zone}":
