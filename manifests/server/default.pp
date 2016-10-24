570a1732110778bbf106ae89ad3c5b112725179b
Johnson Earls <johnson.earls@oracle.com>
2016-10-24 23:25:01 +0000

    Ensure validate_re calls are wrapped in `if` checks to avoid passing undef

--- manifests/server/default.pp
+++ manifests/server/default.pp
@@ -18,23 +18,29 @@ class dns::server::default (
 
   validate_absolute_path( $default_file )
 
-  if $resolvconf {
+  if $resolvconf != undef {
     validate_re( $resolvconf, '^(yes|no)$', 'The resolvconf value is not type of a string yes / no.' )
   }
 
-  if $rootdir {
+  if $rootdir != undef {
     validate_absolute_path( $rootdir )
   }
 
-  validate_re( $enable_zone_write, '^(yes|no|\s*)$', 'The enable_zone_write value is not type of a string yes / no or empty.' )
+  if $enable_zone_write != undef {
+    validate_re( $enable_zone_write, '^(yes|no|\s*)$', 'The enable_zone_write value is not type of a string yes / no or empty.' )
+  }
 
-  validate_re( $enable_sdb, '^(yes|no|1|0|\s*)$', 'The enable_sdb value is not type of a string yes / no / 1 / 0 or empty.' )
+  if $enable_sdb != undef {
+    validate_re( $enable_sdb, '^(yes|no|1|0|\s*)$', 'The enable_sdb value is not type of a string yes / no / 1 / 0 or empty.' )
+  }
 
-  if $keytab_file {
+  if $keytab_file != undef {
     validate_absolute_path( $keytab_file )
   }
 
-  validate_re( $disable_zone_checking, '^(yes|no|\s*)$', 'The disable_zone_checking value is not type of a string yes / no or empty.' )
+  if $disable_zone_checking != undef {
+    validate_re( $disable_zone_checking, '^(yes|no|\s*)$', 'The disable_zone_checking value is not type of a string yes / no or empty.' )
+  }
 
   file { $default_file:
     ensure  => present,
