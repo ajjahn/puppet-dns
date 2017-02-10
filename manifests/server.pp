925f7cd5493edea901fda76d4dbd15ff7c6edd0f
Aaron Peschel <aaron@elementanalytics.com>
2017-02-09 18:42:59 -0800

    Improve Server Class Configurability
    Adds parameters to the server class and passes them to the classes
    instantiated by the server class. This will allow users of the server
    class to set their values in one place, rather than needing to do it for
    every class individually.

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,7 +1,27 @@
 # == Class dns::server
 #
-class dns::server {
-  class { 'dns::server::install': } ->
-  class { 'dns::server::config': } ~>
-  class { 'dns::server::service': }
+class dns::server (
+  $service = $dns::server::params::service,
+
+  $necessary_packages = $dns::server::params::necessary_packages,
+  $ensure_packages    = $dns::server::params::ensure_packages,
+
+  $cfg_dir  = $dns::server::params::cfg_dir,
+  $cfg_file = $dns::server::params::cfg_file,
+  $data_dir = $dns::server::params::data_dir,
+  $owner    = $dns::server::params::owner,
+  $group    = $dns::server::params::group,
+) inherits dns::server::params {
+  class { 'dns::server::install':
+    necessary_packages => $necessary_packages,
+    ensure_packages    => $ensure_packages,
+  } -> class { 'dns::server::config':
+    cfg_dir  => $cfg_dir,
+    cfg_file => $cfg_file,
+    data_dir => $data_dir,
+    owner    => $owner,
+    group    => $group,
+  } ~> class { 'dns::server::service':
+    service => $service,
+  }
 }
