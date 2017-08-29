2ba90801fdfb552bc4b5aa86731816d910556b1b
Angel L. Mateo <amateo@um.es>
2017-08-29 09:47:13 +0200

    Adjust tests to remove ensure parameter for concat::fragment

--- spec/defines/dns__key_spec.rb
+++ spec/defines/dns__key_spec.rb
@@ -18,7 +18,6 @@ describe 'dns::key' do
     it { should contain_file('/etc/bind/bind.keys.d/rspec-key.secret').with_require('Exec[get-secret-from-rspec-key]') }
     it { should contain_concat('/etc/bind/bind.keys.d/rspec-key.key') }
     ['rspec-key.key-header', 'rspec-key.key-secret', 'rspec-key.key-footer'].each do |fragment|
-      it { should contain_concat__fragment(fragment).with_ensure('present') }
       it { should contain_concat__fragment(fragment).with_target('/etc/bind/bind.keys.d/rspec-key.key') }
       it { should contain_concat__fragment(fragment).with_require(['Exec[get-secret-from-rspec-key]', 'File[/etc/bind/bind.keys.d/rspec-key.secret]']) }
     end
@@ -39,7 +38,6 @@ describe 'dns::key' do
     it { should contain_file('/etc/named/bind.keys.d/rspec-key.secret').with_require('Exec[get-secret-from-rspec-key]') }
     it { should contain_concat('/etc/named/bind.keys.d/rspec-key.key') }
     ['rspec-key.key-header', 'rspec-key.key-secret', 'rspec-key.key-footer'].each do |fragment|
-        it { should contain_concat__fragment(fragment).with_ensure('present') }
         it { should contain_concat__fragment(fragment).with_target('/etc/named/bind.keys.d/rspec-key.key') }
         it { should contain_concat__fragment(fragment).with_require(['Exec[get-secret-from-rspec-key]', 'File[/etc/named/bind.keys.d/rspec-key.secret]']) }
     end
