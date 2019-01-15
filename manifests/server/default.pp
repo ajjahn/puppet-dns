44c3c7926bc30bb3ac3b6518ee38626e1df80743
Peter Pouliot <peter@pouliot.net>
2019-01-15 19:05:58 +0000

    more reverting to clear rspec errors until i can build clean

--- manifests/server/default.pp
+++ manifests/server/default.pp
@@ -28,15 +28,17 @@ class dns::server::default (
   }
 
   if $enable_zone_write != undef {
-    assert_type(Pattern[/(^yes|no|\s*)$/], $enable_zone_write) | $a, $b| {
-      fail( 'The enable_zone_writing value is not type of a string yes / no or empty.' )
-    }
+  #  assert_type(Pattern[/(^yes|no|\s*)$/], $enable_zone_write) | $a, $b| {
+  #    fail( 'The enable_zone_writing value is not type of a string yes / no or empty.' )
+  #  }
+     validate_re( $enable_zone_write, '^(yes|no|\s*)$', 'The enable_zone_write value is not type of a string yes / no or empty.' )
   }
 
   if $enable_sdb != undef {
-    assert_type(Pattern[/(^yes|no|\s*)$/], $enable_sdb) | $a, $b| {
-      fail( 'The enable_sdb value is not type of a string yes / no or empty.' )
-    }
+  #  assert_type(Pattern[/(^yes|no|\s*)$/], $enable_sdb) | $a, $b| {
+  #    fail( 'The enable_sdb value is not type of a string yes / no or empty.' )
+  #  }
+    validate_re( $enable_sdb, '^(yes|no|1|0|\s*)$', 'The enable_sdb value is not type of a string yes / no / 1 / 0 or empty.' )
   }
 
   if $keytab_file != undef {
@@ -44,9 +46,10 @@ class dns::server::default (
   }
 
   if $disable_zone_checking != undef {
-    assert_type(Pattern[/(^yes|no|\s*)$/], $disable_zone_checking) | $a, $b| {
-      fail( 'The disable_zone_checking value is not type of a string yes / no or empty.' )
-    }
+    #assert_type(Pattern[/(^yes|no|\s*)$/], $disable_zone_checking) | $a, $b| {
+    #  fail( 'The disable_zone_checking value is not type of a string yes / no or empty.' )
+    #}
+    validate_re( $disable_zone_checking, '^(yes|no|\s*)$', 'The disable_zone_checking value is not type of a string yes / no or empty.' )
   }
 
   file { $default_file:
