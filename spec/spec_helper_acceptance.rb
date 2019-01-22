66fee7b999c164f35d547431db4cb7f7bdf1969f
Peter Pouliot <peter@pouliot.net>
2019-01-22 18:05:44 +0000

    updating typo in spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,16 +4,14 @@ require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 require 'beaker-task_helper'
 
-logger.error("LOADED MYYYYYYYYYY Spec Acceptance Helper")
-
 # install_puppet_on(hosts, options)
 run_puppet_install_helper_on(hosts)
 configure_type_defaults_on(hosts)
 install_ca_certs unless pe_install?
 # install_puppet_agent_on(hosts)
 # install_bolt_on(hosts)
-#install_module_on(hosts)
-#install_module_dependencies_on(hosts)
+# install_module_on(hosts)
+# install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
   # Readable test descriptions
